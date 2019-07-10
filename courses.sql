231fcaf9bbc77e3a517d5f326a2314779fe8c660
acorsiwit <corsia@wit.edu>
2019-07-10 01:23:35 -0400

    Add files via upload

---
+++ courses.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 10, 2019 at 07:22 AM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `courses`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+CREATE TABLE `courses` (
+  `Name` varchar(50) NOT NULL,
+  `Category` varchar(50) NOT NULL,
+  `Semester` varchar(20) NOT NULL,
+  `Credits` int(11) NOT NULL,
+  `School` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`Name`, `Category`, `Semester`, `Credits`, `School`) VALUES
+('Biology1', 'Sciences', 'Fall2018', 4, 'WIT'),
+('Biology2', 'Sciences', 'Fall2018', 4, 'MCPHS'),
+('Chemistry1', 'Sciences', 'Spring2019', 4, 'WIT'),
+('Calculus1', 'Mathematics', 'Fall2018', 4, 'WIT'),
+('Statistics', 'Mathematics', 'Fall2018', 3, 'MCPHS');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
